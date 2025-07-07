7c3af353eba972d14e50cf3bcf5c7f005973ea85
fatima28032004 <farooqetc@yahoo.com>
2025-07-07 19:05:36 +0400

    changes

--- Backend/app/Http/Controllers/EditController.php
+++ Backend/app/Http/Controllers/EditController.php
@@ -9,12 +9,11 @@ use Illuminate\Support\Facades\Auth;
 class EditController extends Controller
 {
     public function edit()
-    {
-        $user = Auth::user(); // gets the currently logged in user
-        if (!$user) {
-            // Optionally, redirect to login or show an error
-            return redirect()->route('signin')->with('status', 'Please sign in to edit your profile.');
-        }
-        return view('edit', compact('user'));
+{
+    $user = \App\Models\User::first();
+    if (!$user) {
+        abort(404, 'No user found in the database.');
     }
+    return view('edit', compact('user'));
+}
 }
