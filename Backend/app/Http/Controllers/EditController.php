630e96da0e5412adff6ab8de3c9cb22235d61baf
fatima28032004 <farooqetc@yahoo.com>
2025-07-07 18:59:06 +0400

    change

--- Backend/app/Http/Controllers/EditController.php
+++ Backend/app/Http/Controllers/EditController.php
@@ -1,14 +1,20 @@
 <?php
 
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class EditController extends Controller
 {
     public function edit()
-{
-    $user = auth()->user(); // or User::find($id) depending on your logic
-    return view('edit', compact('user'));
-}
+    {
+        $user = Auth::user(); // gets the currently logged in user
+        if (!$user) {
+            // Optionally, redirect to login or show an error
+            return redirect()->route('signin')->with('status', 'Please sign in to edit your profile.');
+        }
+        return view('edit', compact('user'));
+    }
 }
