2aded3c27111740d82e4c5eb2eeadafb50a79d6d
fatima28032004 <farooqetc@yahoo.com>
2025-07-07 18:52:52 +0400

    change

--- Backend/app/Http/Controllers/EditController.php
+++ Backend/app/Http/Controllers/EditController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 class EditController extends Controller
 {
     public function show()
-    {
-        return view('edit');
-    }
+{
+    $user = auth()->user(); // or User::find($id) depending on your logic
+    return view('edit', compact('user'));
+}
 }
