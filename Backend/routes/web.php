f17763e8232566675f57ca236da4b5aa80dfcd25
fatima28032004 <farooqetc@yahoo.com>
2025-07-08 12:00:03 +0400

    change

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -17,5 +17,26 @@ Route::get('/signin', function () {
 // Register POST route handled by AuthController
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 
-// Add more routes below as needed, for example:
-// Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+// EditController routes
+Route::get('/edit', [App\Http\Controllers\EditController::class, 'show'])->name('edit.show');
+Route::post('/edit', [App\Http\Controllers\EditController::class, 'update'])->name('edit.update');
+
+// EngineeringController route
+Route::get('/engineering', [App\Http\Controllers\EngineeringController::class, 'index'])->name('engineering.index');
+
+// PageController route
+Route::get('/math', [App\Http\Controllers\PageController::class, 'math'])->name('page.math');
+
+// ProfileController routes
+Route::get('/profile/edit', [App\Http\Controllers\ProfileController::class, 'edit'])->name('profile.edit');
+Route::post('/profile/update', [App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
+Route::delete('/profile/destroy', [App\Http\Controllers\ProfileController::class, 'destroy'])->name('profile.destroy');
+
+// ReviewController route
+Route::post('/review/submit', [App\Http\Controllers\ReviewController::class, 'submit'])->name('review.submit');
+
+// ScienceController route
+Route::get('/science', [App\Http\Controllers\ScienceController::class, 'index'])->name('science.index');
+
+// TechnologyController route
+Route::get('/technology', [App\Http\Controllers\TechnologyController::class, 'show'])->name('technology.show');
